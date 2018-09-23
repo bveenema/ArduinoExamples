ca2761d5d4910a49fb409edfdc3378f3b1c90a90
unknown <bveenema@gmail.com>
2018-09-23 12:00:06 -0400

    Fix naming error

--- Particle Firmware/src/config.h
+++ Particle Firmware/src/config.h
@@ -35,7 +35,7 @@
 #define TIME_BETWEEN_FLUSHES 300000 // 5 minutes
 #define FLUSH_VOLUME 150 //ml
 #define CLEANING_FLOW_RATE 3000 // ml/min
-#define CLEANING_VOlUME_PER_PULSE 200 //ml
+#define CLEANING_VOLUME_PER_PULSE 200 //ml
 #define CLEANING_PULSE_INTERVAL 10000 // time bewteen start of pulses (10 sec)
 #define CLEANING_CYCLE_DURATION 600000 // duration of cleaning cycle (10 min)
 
