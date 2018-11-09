8cf72e08520dea18b86a22b0622546054b89a3ca
unknown <bveenema@gmail.com>
2018-11-09 05:06:20 -0500

    Fix hasLiquid inverted

--- Particle Firmware/src/LiquidSensor.cpp
+++ Particle Firmware/src/LiquidSensor.cpp
@@ -15,7 +15,7 @@ void liquidSensor::update(){
   // If signal is low, there is no liquid
   if(millis() - _lastRead > 1000){
     _lastRead = millis();
-    _hasLiquid = IOExp.digitalRead(_pin);
+    _hasLiquid = !IOExp.digitalRead(_pin);
   }
 }
 
